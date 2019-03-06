1f4bcf93cd0c06c88f0cf6ddd71527ed5fa2f0e5
Bjorn Hjortsberg <bjorn.hjortsberg@gmail.com>
2019-03-06 22:47:15 +0100

    Rename to nubecli

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="Cloud tool",
-    version="0.2.0",
-    description="Cloud command line tool",
+    name="nubecli",
+    version="0.3.0",
+    description="Cloud (VPS) command line tool",
     author="Bjorn Hjortsberg",
     packages=[".", "config"],
     entry_points={
         'console_scripts': [
-            'ccli = ccli:main',
-        ], 
+            'nubecli=nubecli:main',
+        ],
     }
 )
 
